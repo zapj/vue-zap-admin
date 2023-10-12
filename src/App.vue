c734549de9a9a666e7c0296aec81957fd04bc23c
zap <z@zap.cn>
2023-10-12 16:15:29 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,29 @@
 <script setup>
+import LeftSidebar from "@/components/LeftSidebar.vue";
+import TopMenu from "@/components/TopMenu.vue";
+import {ref,onMounted} from "vue";
+onMounted(()=>{
 
+})
+const leftSidebar = ref()
+const closeMenu = ()=>{
+  console.log('closeMenu')
+  leftSidebar.value.closeMenu()
+}
 </script>
 
 <template>
-  <div class="common-layout">
     <el-container>
-      <el-aside width="200px">
-        <el-scrollbar>
-          <el-menu :default-openeds="['1', '3']">
-            <el-sub-menu index="1">
-              <template #title>
-                <el-icon><message /></el-icon>Navigator One
-              </template>
-              <el-menu-item-group>
-                <template #title>Group 1</template>
-                <el-menu-item index="1-1">Option 1</el-menu-item>
-                <el-menu-item index="1-2">Option 2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group 2">
-                <el-menu-item index="1-3">Option 3</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="1-4">
-                <template #title>Option4</template>
-                <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-            <el-sub-menu index="2">
-              <template #title>
-                <el-icon><icon-menu /></el-icon>Navigator Two
-              </template>
-              <el-menu-item-group>
-                <template #title>Group 1</template>
-                <el-menu-item index="2-1">Option 1</el-menu-item>
-                <el-menu-item index="2-2">Option 2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group 2">
-                <el-menu-item index="2-3">Option 3</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="2-4">
-                <template #title>Option 4</template>
-                <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-            <el-sub-menu index="3">
-              <template #title>
-                <el-icon><setting /></el-icon>Navigator Three
-              </template>
-              <el-menu-item-group>
-                <template #title>Group 1</template>
-                <el-menu-item index="3-1">Option 1</el-menu-item>
-                <el-menu-item index="3-2">Option 2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group 2">
-                <el-menu-item index="3-3">Option 3</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="3-4">
-                <template #title>Option 4</template>
-                <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-          </el-menu>
-        </el-scrollbar>
-      </el-aside>
+      <LeftSidebar ref="leftSidebar" @closeMenu="closeMenu"></LeftSidebar>
       <el-container>
-        <el-header>Header</el-header>
+          <TopMenu></TopMenu>
         <el-main>
-          <RouterView />
+          <RouterView></RouterView>
         </el-main>
         <el-footer>Footer</el-footer>
       </el-container>
     </el-container>
-  </div>
-
-
+    <el-backtop :right="10" :bottom="100" />
 </template>
 
 <style scoped>
---
+++ src/components/TopMenu.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-header>
+    <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        :ellipsis="false"
+        @select="handleSelect"
+    >
+      <el-menu-item index="-1" >
+        <span style="width: 10px;text-align: center" ><el-icon><Back></Back></el-icon></span>
+      </el-menu-item>
+      <el-menu-item index="0">LOGO</el-menu-item>
+      <div class="flex-grow" />
+      <el-menu-item index="1">Processing Center</el-menu-item>
+      <el-sub-menu index="2">
+        <template #title>Workspace</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-sub-menu index="2-4">
+          <template #title>item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+    </el-menu>
+
+
+
+
+  </el-header>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import {Back} from "@element-plus/icons-vue";
+
+const activeIndex = ref('1')
+const handleSelect = (key, keyPath) => {
+  console.log(key, keyPath)
+  if(key === "-1"){
+    console.log('emit')
+    emit('closeMenu');
+  }
+}
+
+
+const emit = defineEmits(['closeMenu']);
+
+</script>
+
+<style>
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
